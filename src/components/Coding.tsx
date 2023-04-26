5cf4a71faa89a74fc0e13af1bbc06118ec956b05
Tanzim Mokammel <mtanzim@gmail.com>
2023-04-26 19:39:32 -0400

    multiple options for data set

--- src/components/Coding.tsx
+++ src/components/Coding.tsx
@@ -7,17 +7,25 @@ import {
 } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import colors from "../colors.json";
+import { useState } from "react";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 interface Props {
-  months: number;
   topN: number;
-  data: Array<{ language: string; percentage: number }>;
+  dataSet: Array<{
+    months: number;
+    data: Array<{ language: string; percentage: number }>;
+  }>;
 }
 
-export default function Coding({ months, data, topN }: Props) {
-  const slicedData = data.slice(0, topN);
+export default function Coding({ dataSet, topN }: Props) {
+  const [curData, setCurData] = useState(dataSet?.[0]);
+  if (!curData) {
+    return null;
+  }
+  const options = dataSet.map((d) => d.months);
+  const slicedData = curData?.data?.slice(0, topN);
   const chartData: ChartData<"pie", number[], string> = {
     labels: slicedData.map((d) => d.language),
     datasets: [
@@ -30,9 +38,22 @@ export default function Coding({ months, data, topN }: Props) {
   };
   return (
     <div>
-      <h1 className="text-4xl mb-4 mt-4">
-        Top languages used in the last {months} months
-      </h1>
+      <h1 className="text-4xl mb-4 mt-4">My top languages used</h1>
+      <div className="tabs mb-4 mt-4">
+        {options.map((month) => (
+          <button
+            key={month}
+            onClick={() => setCurData(dataSet.find((d) => d.months === month))}
+            className={
+              month === curData.months
+                ? "tab tab-bordered tab-active"
+                : "tab tab-bordered"
+            }
+          >
+            {month} Months
+          </button>
+        ))}
+      </div>
       <Pie data={chartData} />
     </div>
   );
