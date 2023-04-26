872c394b70af76d850c9e7977214f6a595fce407
Tanzim Mokammel <mtanzim@gmail.com>
2023-04-26 19:00:43 -0400

    .

--- src/components/Coding.tsx
+++ src/components/Coding.tsx
@@ -12,24 +12,26 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 interface Props {
   months: number;
+  topN: number;
   data: Array<{ language: string; percentage: number }>;
 }
 
-export default function Coding({ months, data }: Props) {
+export default function Coding({ months, data, topN }: Props) {
+  const slicedData = data.slice(0, topN);
   const chartData: ChartData<"pie", number[], string> = {
-    labels: data.map((d) => d.language),
+    labels: slicedData.map((d) => d.language),
     datasets: [
       {
         label: "",
-        data: data.map((d) => d.percentage),
-        backgroundColor: data.map((d) => colors?.[d?.language]?.color),
+        data: slicedData.map((d) => d.percentage),
+        backgroundColor: slicedData.map((d) => colors?.[d?.language]?.color),
       },
     ],
   };
   return (
     <div>
       <h1 className="text-4xl mb-4 mt-4">
-        Languages used in the last {months} months
+        Top languages used in the last {months} months
       </h1>
       <Pie data={chartData} />
     </div>
