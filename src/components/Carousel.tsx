ff89cee4ef58e2655af8cb017a71e6cd6e1aef16
Tanzim Mokammel <mtanzim@gmail.com>
2023-06-03 12:28:29 -0400

    add image captions

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,17 +3,33 @@ import React from "react";
 export type CarouselImage = {
   url: string;
   alt: string;
+  title: string;
+  desc?: string;
 };
 type Props = {
   images: CarouselImage[];
 };
 
+export const CarouselImageItem = ({ img }: { img: CarouselImage }) => {
+  return (
+    <div className="flex flex-col">
+      <img src={img.url} alt={img.alt} />
+      <div className="divider"></div>
+      <div>
+        <p className="ml-4 text-base">{img.title}</p>
+        <p className="ml-8 text-xs">{img.desc ?? ""}</p>
+      </div>
+    </div>
+  );
+};
+
 export default function Carousel({ images }: Props) {
+  const len = images.length;
   return (
     <div className="carousel carousel-center rounded-box">
       {images.map((img: CarouselImage) => (
-        <div key={img.alt} className="carousel-item w-1/3">
-          <img src={img.url} alt={img.alt} />
+        <div key={img.alt} className={`carousel-item w-1/3`}>
+          <CarouselImageItem img={img} />
         </div>
       ))}
     </div>
