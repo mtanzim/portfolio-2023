929696fc03a769446fc0f5ba5f5e8186bb7a1126
Tanzim Mokammel <mtanzim@gmail.com>
2023-06-10 10:57:16 -0400

    carousel responsive

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -11,32 +11,18 @@ type Props = {
   customImgClasses?: HTMLProps<HTMLElement>["className"];
 };
 
-export const CarouselImageItem = ({
-  img,
-  customImgClasses,
-}: {
-  img: CarouselImage;
-  customImgClasses?: HTMLProps<HTMLElement>["className"];
-}) => {
-  return (
-    <div className="flex flex-col mx-auto">
-      <img className={customImgClasses ?? ""} src={img.url} alt={img.alt} />
-      <div className="divider"></div>
-      <p className="ml-4 text-base">{img.title}</p>
-      <p className="ml-4 text-xs">{img.desc ?? ""}</p>
-    </div>
-  );
-};
-
 export default function Carousel({ images, customImgClasses }: Props) {
   if (images.length !== 3) {
     throw new Error("ugly, please fix");
   }
   return (
-    <div className="carousel carousel-center rounded-box">
+    <div className="rounded-box md:flex justify-center">
       {images.map((img: CarouselImage) => (
-        <div key={img.alt} className="carousel-item w-1/3">
-          <CarouselImageItem customImgClasses={customImgClasses} img={img} />
+        <div key={img.alt} className="mb-4 md:mb-0 md:w-1/3">
+          <img className={customImgClasses ?? ""} src={img.url} alt={img.alt} />
+          <div className="divider"></div>
+          <p className="ml-4 text-base">{img.title}</p>
+          <p className="ml-4 text-xs">{img.desc ?? ""}</p>
         </div>
       ))}
     </div>
