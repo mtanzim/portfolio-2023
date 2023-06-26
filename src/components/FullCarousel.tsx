4253d91a1437d24350bc94889db4915fde57ad48
Tanzim Mokammel <20617008+mtanzim@users.noreply.github.com>
2023-06-25 23:12:33 -0400

    add travel section (#18)
    * add travel pics
    
    * styles
    
    * update travel section
    
    * this is a lot of images

---
+++ src/components/FullCarousel.tsx
@@ -0,0 +1,44 @@
+export type FullCarouselImage = {
+  url: string;
+  title: string;
+  description?: string;
+};
+
+type Props = {
+  images: FullCarouselImage[];
+  sectionPrefix: string;
+};
+export default function FullCarousel({ images, sectionPrefix }: Props) {
+  function genSlideName(idx: number) {
+    return `${sectionPrefix}-slide-${idx + 1}`;
+  }
+  return (
+    <>
+      <div className="carousel w-full h-[256px] md:h-[768px]">
+        {images.map((image, idx) => (
+          <div
+            id={genSlideName(idx)}
+            className="carousel-item w-full h-full relative"
+          >
+            <img
+              src={image.url}
+              alt={image.title}
+              className="w-full object-contain py-12"
+            />
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center w-full py-2 gap-2">
+        {images.map((img, idx) => (
+          <a
+            href={`#${genSlideName(idx)}`}
+            className="tooltip"
+            data-tip={img.title}
+          >
+            <button className="btn btn-xs">{idx + 1}</button>
+          </a>
+        ))}
+      </div>
+    </>
+  );
+}
