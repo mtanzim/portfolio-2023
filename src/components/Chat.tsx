8bf2c5d7cab070a93bddcde73ca8f4aa0caab825
Tanzim Mokammel <20617008+mtanzim@users.noreply.github.com>
2023-07-30 13:25:29 -0400

    Chat (#19)
    * add badge for beta
    
    * starting to look nice
    
    * getting cors error from worker
    
    * .
    
    * ok this works well
    
    * info
    
    * info dropdown
    
    * .
    
    * looking better
    
    * ok I think I'm done for now
    
    * link the homepage

---
+++ src/components/Chat.tsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+
+async function callAPI(question: string): Promise<unknown> {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    question,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+  };
+
+  return fetch(
+    "https://personal-portfolio-chat-worker.mtanzim.workers.dev",
+    requestOptions
+  ).then((response) => response.json());
+}
+
+const sampleQuestions = [
+  "What can you tell me?",
+  "Describe Tanzim's story towards web dev",
+  "Summarize Tanzim's resume in 500 words",
+  "What are some of Tanzim's hobbies?",
+];
+
+export const Chat: React.FC<{}> = () => {
+  const [query, setQuery] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [res, setRes] = useState<null | string>(null);
+  const [err, setErr] = useState<null | string>(null);
+
+  const onSubmit = async (submittedQuery: string | null) => {
+    if (!submittedQuery) {
+      return;
+    }
+    setErr(null);
+    setRes(null);
+
+    setLoading(true);
+
+    try {
+      const apiRes = (await callAPI(submittedQuery)) as string;
+      setRes(apiRes);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setErr(err.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex">
+        <input
+          type="text"
+          placeholder="Ask your question here"
+          className="input input-bordered input-primary w-full max-w-lg"
+          value={query || ""}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          disabled={!!loading}
+          onClick={() => onSubmit(query)}
+          className="ml-4 btn btn-primary"
+        >
+          Ask away
+        </button>
+        {!loading && (
+          <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className="ml-4 btn btn-secondary">
+              Sample Questions
+            </label>
+            <ul
+              tabIndex={0}
+              className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52"
+            >
+              {sampleQuestions.map((q) => (
+                <li key={q}>
+                  <button
+                    disabled={loading}
+                    onClick={() => {
+                      setQuery(q);
+                      onSubmit(q);
+                    }}
+                  >
+                    {q}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      {query && (
+        <div className="mockup-code mt-8">
+          <pre data-prefix=">">
+            <code>gippity ask {query}</code>
+          </pre>
+          {loading && (
+            <>
+              <pre data-prefix=">" className="text-warning">
+                <code>calling the gippity, this can take a while...</code>
+              </pre>
+              <progress className="progress w-96 progress-info ml-8"></progress>
+            </>
+          )}
+          {res && (
+            <>
+              <pre data-prefix=">" className="text-info">
+                gippity gave the following answer:
+              </pre>
+              <div className="m-8">
+                <code>{res}</code>
+              </div>
+            </>
+          )}
+          {err && (
+            <pre data-prefix=">" className="bg-warning text-warning-content">
+              <code>{err}</code>
+            </pre>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
