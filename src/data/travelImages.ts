26e37e126c1a581662af5ff753ee5ffe0bd7cd78
Tanzim Mokammel <mtanzim@gmail.com>
2023-11-04 12:44:42 -0400

    style, remove local images

--- src/data/travelImages.ts
+++ src/data/travelImages.ts
@@ -1,8 +1,9 @@
 const s3Path = "https://tm-photo-portfolio.s3.amazonaws.com/2023/";
+// TODO: add titles and description to images
 const fileNames = [
-  "DSC09679.jpg",
   "DSC09717.jpg",
   "DSC09760.jpg",
+  "DSC09679.jpg",
   "DSC09659.jpg",
   "DSC09875.jpg",
   "DSC00044.jpg",
@@ -12,13 +13,11 @@ const fileNames = [
   "DSC08625.jpg",
   "DSC08707.jpg",
   "DSC08920.jpg",
-  "DSC08943.jpg",
   "DSC08947.jpg",
   "DSC08990.jpg",
   "DSC09102.jpg",
   "DSC09220.jpg",
   "DSC09526.jpg",
-  "DSC09603.jpg",
 ];
 
 export const travelImagePaths2023 = fileNames.map((name) => `${s3Path}${name}`);
