8f83512b38b8d5540928d6b455e1e108b7d60de7
Tanzim Mokammel <mtanzim@gmail.com>
2024-01-13 19:48:04 -0500

    remove other deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mtanzim.com",
