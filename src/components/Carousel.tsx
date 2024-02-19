b851619a2a61ef7e78531f066dc649900f19f447
Tanzim Mokammel <mtanzim@gmail.com>
2024-02-19 14:18:08 -0500

    start creating clips carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -28,3 +28,33 @@ export default function Carousel({ images, customImgClasses }: Props) {
     </div>
   );
 }
+
+export type CarouselClip = {
+  url: string;
+  alt: string;
+  title: string;
+  desc?: string;
+};
+type VideoProps = {
+  clips: CarouselClip[];
+};
+
+export function VideoCarousel({ clips }: VideoProps) {
+  if (clips.length !== 3) {
+    throw new Error("ugly, please fix");
+  }
+  return (
+    <div className="rounded-box md:flex justify-center">
+      {clips.map((img: CarouselClip) => (
+        <div key={img.alt} className="mb-4 md:mb-0 md:w-1/3">
+          <video controls width="250">
+            <source src={img.url} type="video/webm" />
+          </video>
+          <div className="divider"></div>
+          <p className="ml-4 text-base">{img.title}</p>
+          <p className="ml-4 text-xs">{img.desc ?? ""}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
