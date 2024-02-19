4e945f74f275439193dd2ac15eb35721c793e634
Tanzim Mokammel <mtanzim@gmail.com>
2024-02-19 14:49:51 -0500

    working video clips from S3

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -40,19 +40,20 @@ type VideoProps = {
 };
 
 export function VideoCarousel({ clips }: VideoProps) {
-  if (clips.length !== 3) {
-    throw new Error("ugly, please fix");
-  }
   return (
-    <div className="rounded-box md:flex justify-center">
-      {clips.map((img: CarouselClip) => (
-        <div key={img.alt} className="mb-4 md:mb-0 md:w-1/3">
-          <video controls width="250">
-            <source src={img.url} type="video/webm" />
+    <div className="rounded-box">
+      {clips.map((clip: CarouselClip) => (
+        <div className="mb-4 md:mb-0">
+          <p className="ml-4 text-base">{clip.title}</p>
+          <p className="ml-4 text-xs mb-4">{clip.desc ?? ""}</p>
+          <video key={clip.url} controls>
+            <source src={clip.url} type="video/webm" />
+            Sorry, your browser doesn't support embedded videos, but don't
+            worry, you can
+            <a href={clip.url}>download it</a>
+            and watch it with your favorite video player!
           </video>
           <div className="divider"></div>
-          <p className="ml-4 text-base">{img.title}</p>
-          <p className="ml-4 text-xs">{img.desc ?? ""}</p>
         </div>
       ))}
     </div>
